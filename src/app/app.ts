c8c9a01d725559935ef29ba05ec8917e6ee2aacf
xenixux <kseniakonnova.ux@gmail.com>
2026-02-18 19:17:03 +0100

    Front & Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {BloggingComponent} from './components/blogging/blogging.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BloggingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/interfaces/inotice.ts
@@ -0,0 +1,6 @@
+export interface INotice {
+    title: string;
+    url_foto: string;
+    notice_content: string;
+    date: Date;
+}
